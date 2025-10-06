cf6d84204c99eafc39f8b08f73c4d3cf555a5b6c
filozofkubik <jakub.santora.g@gmail.com>
2025-10-06 19:45:50 +0200

    nnn

---
+++ SortArray/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace SortArray
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!   b");
+        }
+    }
+}
