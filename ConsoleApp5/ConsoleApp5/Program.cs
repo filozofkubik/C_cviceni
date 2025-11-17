4aa9824ce75914b9a0b9d66eb7589868bb723eb4
filozofkubik <jakub.santora.g@gmail.com>
2025-11-17 13:24:07 +0100

    hwdwuduh

---
+++ ConsoleApp5/ConsoleApp5/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace řetízkový_graf
+{
+    internal class Program
+    {
+
+        static void Main(string[] args)
+        {
+            int pocetLidi = Convert.ToInt32(Console.ReadLine());
+            int[,] graf = new int[pocetLidi, pocetLidi];
+            string vstupniData = Console.ReadLine();
+            string[] dvojice = vstupniData.Split();
+
+
+
+
+            double[,] matrix = new double[pocetLidi + 1, pocetLidi + 1];
+
+            for (int i = 0; i < dvojice.Length; i++)
+            {
+                string[] par = dvojice[i].Split('-');
+                int a = Convert.ToInt32(par[0]);
+                int b = Convert.ToInt32(par[1]);
+                matrix[a, b] = 1;
+                matrix[b, a] = 1;
+
+            }
+        }
+
+    }
+}
+
+    }
+}
