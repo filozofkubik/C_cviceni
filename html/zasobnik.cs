7d105e379ea4d77ac83b571dfb4b1604b259487b
filozofkubik <jakub.santora.g@gmail.com>
2026-01-18 19:59:23 +0100

    zasobnik

---
+++ html/zasobnik.cs
@@ -0,0 +1,34 @@
+﻿
+class Program
+{
+    static bool Vozavorkovanospravne(string vstup)
+    {
+        Stack<char> zasobnik = new Stack<char>();
+        foreach (char znak in vstup)
+        {
+            if (znak == '(' || znak == '[' || znak == '{')
+            {
+                zasobnik.Push(znak);
+            }
+            else if (znak ==')' || znak == ']' || znak == '}')
+            {
+                if (zasobnik.Count == 0) {
+                    return false;
+            }
+        char prectenej = zasobnik.Pop();
+        if (znak == ')' && prectenej != '(') {
+            return false ;}
+                if (znak == ']' && prectenej != '[')
+                {
+                    return false;
+                }
+                if (znak == '}' && prectenej != '{')
+                {
+                    return false;
+                }
+            }
+        }
+        return zasobnik.Count == 0;
+    }
+
+}
